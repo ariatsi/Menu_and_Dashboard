4697255730c10abc59018aaef759735856435b58
ariatsi <92860360+ariatsi@users.noreply.github.com>
2024-04-03 17:06:58 +0200

    adding version control

---
+++ app/src/main/java/fr/itds/menu_and_dashboard/DashboardActivity.java
@@ -0,0 +1,85 @@
+package fr.itds.menu_and_dashboard;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.widget.Toolbar;
+
+public class DashboardActivity extends AppCompatActivity {
+    TextView welcomeText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_dashboard);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Récupérer les éléments de l'interface utilisateur
+        welcomeText = findViewById(R.id.welcomeText);
+
+        // Configure la Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Récupérer les données de l'intent
+        Intent intent = getIntent();
+        String lastName = intent.getStringExtra("lastName");
+        String firstName = intent.getStringExtra("firstName");
+        String email = intent.getStringExtra("email");
+        // Token peut être stocké dans SharedPreferences pour un accès global
+        String token = intent.getStringExtra("token");
+
+        // Stocker le token dans SharedPreferences
+        SharedPreferences sharedPreferences = getSharedPreferences("appData", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("token", token);
+        editor.apply();
+
+        // Afficher le message de bienvenue
+        welcomeText.setText(String.format("Bienvenue %s %s !", firstName, lastName));
+        toolbar.setTitle("Logged in as " + email);
+        toolbar.setTitleTextColor(Color.WHITE);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.dashboard_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.logout) {
+                // Effacer le token et déconnecter l'utilisateur
+                SharedPreferences sharedPreferences = getSharedPreferences("appData", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.remove("token");
+                editor.apply();
+
+                Intent intent = new Intent(DashboardActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                return true;
+        } else {
+            // Ajoutez des cas pour les autres items du menu si nécessaire
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/fr/itds/menu_and_dashboard/MainActivity.java
@@ -0,0 +1,123 @@
+package fr.itds.menu_and_dashboard;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText username, password;
+    Button login;
+    ProgressDialog progressDialog;
+    RequestQueue requestQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        login = findViewById(R.id.login);
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Logging in... Please wait.");
+        progressDialog.setCancelable(false);
+
+        requestQueue = Volley.newRequestQueue(this);
+
+        login.setOnClickListener(v -> {
+            String userVar = username.getText().toString();
+            String passVar = password.getText().toString();
+            if (userVar.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "Username cannot be blank", Toast.LENGTH_SHORT).show();
+            } else if (passVar.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "Password cannot be blank", Toast.LENGTH_SHORT).show();
+            } else {
+                loginRequest(userVar, passVar);
+            }
+        });
+    }
+
+    private void loginRequest(String userVar, String passVar) {
+        String loginUrl = "https://temp.itds.fr/login_tst.php"; // your server's URL
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, loginUrl,
+                response -> {
+                    progressDialog.dismiss();
+                    try {
+                        JSONObject jsonObject = new JSONObject(response);
+                        int status = jsonObject.getInt("status");
+
+                        if (status == 200) { // Success
+                            Toast.makeText(MainActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+
+                            // Proceed to next activity or dashboard
+                            String userId = jsonObject.getString("id");
+                            String lastName = jsonObject.getString("lastName");
+                            String firstName = jsonObject.getString("firstName");
+                            String email = jsonObject.getString("email");
+                            String role = jsonObject.getString("role");
+                            String token = jsonObject.getString("token");
+
+                            Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
+                            intent.putExtra("userId", userId);
+                            intent.putExtra("lastName", lastName);
+                            intent.putExtra("firstName", firstName);
+                            intent.putExtra("email", email);
+                            intent.putExtra("role", role);
+                            intent.putExtra("token", token);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Incorrect username or password", Toast.LENGTH_LONG).show();
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        Toast.makeText(MainActivity.this, "Failed to parse server response", Toast.LENGTH_LONG).show();
+                    }
+                },
+                error -> {
+                    progressDialog.dismiss();
+                    Toast.makeText(MainActivity.this, "Failed to connect to server. Please check your internet connection and try again.", Toast.LENGTH_LONG).show();
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("email", userVar);
+                params.put("password", passVar);
+                return params;
+            }
+        };
+
+        progressDialog.show();
+        requestQueue.add(stringRequest);
+    }
+}
